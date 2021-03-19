b721d2141552b6e5e7682933a3dfc5559b532683
JerryDanks <jerrydanks@outlook.com>
2021-03-19 17:01:53 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MvcCoreEFSamples.Models;
+using MvcCoreEFSamples.Models.SchoolViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreEFSamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            Stack<string> testStr = new Stack<string>();
+            testStr.Push("one");
+
+            LinkedList<string> testLinked = new LinkedList<string>();
+            //testLinked.AddBefore(new LinkedListNode<string>())
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
